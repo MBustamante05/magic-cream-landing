a90b8ef154219e1a56ca3aeb73c5d1051abac6d7
MBustamante <mosquerabustamante88@gmail.com>
2026-01-25 21:12:13 -0500

    Enhance Hero component layout and styling for improved responsiveness and user experience

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -5,32 +5,38 @@ import { useState } from "react";
 function Hero() {
   const [isBtnLearnMoreHover, setIsBtnLearnMoreHover] = useState(false);
   return (
-    <div className="min-h-screen overflow-hidden">
+    <div className="max-h-screen lg:min-h-screen overflow-hidden">
       <NavBar />
-      <section className="relative mt-25 md:mt-1 bg-blue min-h-[85vh] rounded-3xl flex flex-col justify-center items-center text-center px-4">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-[13rem] font-bold text-white mb-10 leading-none ">
+      <section className="relative mt-25 md:mt-1 bg-blue py-20 lg:py-0 lg:min-h-[85vh] rounded-3xl flex flex-col justify-center items-center text-center px-4">
+        <div className="max-w-7xl mx-auto z-20 lg:z-10">
+          <h1
+            className={`text-[clamp(2rem,13vw,13rem)] font-bold text-white mb-10 leading-none`}
+          >
             Magic Cream
           </h1>
-          <div className="flex justify-between">
-            <p className="text-white text-lg mb-8 max-w-lg text-left">
+          <div className="lg:flex justify-between px-3 sm:px-7 md:px-13 lg:px-0">
+            <p className="text-white xl:text-lg mb-8 w-full lg:max-w-sm xl:max-w-lg text-left font-medium md:font-normal mx-auto lg:mx-0">
               Experience the enchanting taste of Magic Cream, the ice cream that
               brings joy and wonder to every scoop. Made with the finest
               ingredients and a touch of magic, it's more than just a
               dessert—it's an adventure for your taste buds!
             </p>
-            <div className="flex items-center gap-3">
-              <button className="border border-white text-white font-bold px-6 py-4 rounded-full hover:bg-white hover:text-blue hover:scale-105 transition-all duration-300 cursor-pointer">
+            <div className="flex flex-col sm:flex-row sm:items-center gap-3 w-full lg:w-auto justify-center lg:justify-start mx-auto lg:mx-0">
+              <button className="border border-white text-white font-bold px-4 py-3 sm:px-6 sm:py-4 rounded-full hover:bg-white hover:text-blue hover:scale-105 transition-all duration-300 cursor-pointer">
                 Explore Products
               </button>
               <button
-                className="flex items-center bg-white text-blue font-bold px-6 py-4 rounded-full hover:scale-105 transition-all duration-300 cursor-pointer"
+                className="flex items-center justify-center gap-2
+                bg-white text-blue font-bold
+                px-4 py-3 sm:px-6 sm:py-4
+                rounded-full hover:scale-105
+                transition-all duration-300 cursor-pointer"
                 onMouseEnter={() => setIsBtnLearnMoreHover(true)}
                 onMouseLeave={() => setIsBtnLearnMoreHover(false)}
               >
-                Learn More
+                <span className="text-center">Learn More</span>
                 <ArrowRight
-                  className={`inline-block ml-2  ${isBtnLearnMoreHover ? "translate-x-2" : ""} transition-all duration-300`}
+                  className={`${isBtnLearnMoreHover ? "translate-x-2" : ""} transition-all duration-300`}
                 />
               </button>
             </div>
@@ -38,7 +44,7 @@ function Hero() {
         </div>
         <img
           src="/main-cream2.png"
-          className="absolute bottom-0 w-1/3"
+          className="absolute bottom-0 lg:z-20 z-10 md:h-full lg:h-auto xl:w-1/3 object-contain"
           alt=""
         />
       </section>
